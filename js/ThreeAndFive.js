0ac023e57ba65238f2768463a1e3db2bca43ee88
Devan Barger <DevanBarger33@gmail.com>
2017-08-27 16:46:29 -0500

    cleaned up the functionality and worked out a bug in the calculation logic.

--- js/ThreeAndFive.js
+++ js/ThreeAndFive.js
@@ -1,23 +1,27 @@
 function isMultiple(number){
 	// Checks to see if the given number is a multiple of 3 or 5
 	// then returns a boolean
-	if(number%3 || number%5 === 0){
+	if((number%3 == 0) || (number%5 == 0)){
 		return true; 
+	}else{
+		return false; 
 	}
 }
 
 $("#range").blur(function(){
-	var iterator = 0; 
 	var sum = 0;
 	var range = document.getElementById("range").value; 
 	
-	while (iterator < range){
-		if (isMultiple(iterator)){
-			sum = sum + iterator; 
+	if (!(range <= 0)){
+		for(i=1; i<range; i++){ 
+			if (isMultiple(i)){
+				sum += i; 
+			}
 		}
-	iterator ++; 
+		$("#solution").text(sum); 
+	}else{
+		alert("Invalid limit! Please enter a value > 0."); 
 	}
-	alert(sum); 
 }); 
 
 
